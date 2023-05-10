4e502236b16923c218016cc3e38770ae85a093f6
gustavoribeirodev@gmail.com <gustavoribeirodev@gmail.com>
2023-05-10 07:36:41 -0300

    Added soma Javascript context

---
+++ main.js
@@ -0,0 +1,10 @@
+var language = document.getElementById(options);
+var langoptions = document.getElementById(LangOptions);
+options.addEventlistener("click", function) {
+  if (LangOptions.style.display = "block") {
+    LangOptions.style.display = "none"
+    else {
+      LangOptions.style.display = "block"
+    }
+  }
+}
