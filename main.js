dd278943e2c307ce2f26a99b747345ee24fd8ccd
Bugaboo2000 <gustavoribeirodev@gmail.com>
2023-05-15 23:15:43 -0300

    javascript fix

--- main.js
+++ main.js
@@ -1,10 +1,2 @@
-var language = document.getElementById(options);
-var langoptions = document.getElementById(LangOptions);
-options.addEventlistener("click", function) {
-  if (LangOptions.style.display = "block") {
-    LangOptions.style.display = "none"
-    else {
-      LangOptions.style.display = "block"
-    }
-  }
-}
+var language = document.getElementById(Language);
+var langoptions = document.getElementById(Langoptions);
