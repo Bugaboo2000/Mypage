896c3e527bd4650112a266e8060fad7c1ddab4f8
Bugaboo2000 <gustavoribeirodev@gmail.com>
2023-05-28 07:38:39 -0300

    Condition attempts

--- main.js
+++ main.js
@@ -2,3 +2,5 @@ var language = document.getElementById(language);
 var langoptions = document.getElementById(langoptions);
 
 /* I have plans to add some Dom manipulation, like scroll conditions and interactions in the page */
+
+document.addEventListener
