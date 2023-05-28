903d4a9c863f19237330ad8795a4fad55e865f6d
Bugaboo2000 <gustavoribeirodev@gmail.com>
2023-05-27 22:50:22 -0300

    Small reminder

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
 var language = document.getElementById(language);
 var langoptions = document.getElementById(langoptions);
 
+/* I have plans to add some Dom manipulation, like scroll conditions and interactions in the page */
