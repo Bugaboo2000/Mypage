09dc381fcb6ea6b0ba1fbdfb073a4cbf97110d06
Bugaboo2000 <gustavoribeirodev@gmail.com>
2023-05-30 22:50:38 -0300

    Added DOM manipulation

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
-var language = document.getElementById(language);
-var langoptions = document.getElementById(langoptions);
-
-/* I have plans to add some Dom manipulation, like scroll conditions and interactions in the page */
-
-document.querySelector("#showcase").innerHTML = "In this page you gonna see some previews of my projects:";
+var subtitle = document.querySelector("h2");
+var textInfo = document.querySelector(".textInfo");
+var showcase = document.querySelector(".showcase");
+    showcase.innerHTML = "In this page you gonna see some previews of my projects"; 
+    console.log(navigator.language);
+        if (navigator.language = 'pt-BR') {
+            subtitle.innerHTML = "Sobre Mim";
+            showcase.innerHTML = "Nessa página você poderá ver Previews dos meus projetos";
+        }
