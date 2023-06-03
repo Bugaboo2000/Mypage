5641d48f08fbfb9589863785a7f5f225aeacd88c
Bugaboo2000 <gustavoribeirodev@gmail.com>
2023-06-02 23:13:29 -0300

    Updated translation and some tweaks

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 var subtitle = document.querySelector("h2");
-var textInfo = document.querySelector(".textInfo");
+var textInfo = document.querySelector(".Textinfo");
 var showcase = document.querySelector(".showcase");
     showcase.innerHTML = "In this page you gonna see some previews of my projects"; 
     console.log(navigator.language);
         if (navigator.language = 'pt-BR') {
             subtitle.innerHTML = "Sobre Mim";
             showcase.innerHTML = "Nessa página você poderá ver Previews dos meus projetos";
+            textInfo.innerHTML = "Olá o meu nome é Gustavo Ribeiro, e essa é a minha página principal. Sou um desenvolvedor entusiasta, gosto de programar por paixão, por curiosidade e pela ciência. Abaixo tenho listado um dos meus projetos mais conhecidos:"
+            
         }
