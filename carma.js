88ebcd94a350ddb031beedf6ee9c826d3ffd80ac
Bugaboo2000 <gustavoribeirodev@gmail.com>
2023-07-10 10:39:24 -0300

    updated page

--- carma.js
+++ carma.js
@@ -1,2 +1,14 @@
 var info = document.querySelector("p");
 info.innerHTML = "Esse projeto foi desenvolvido no intuito de concertar as incompatibilidades e roda-lô em Hardwares mais modernos que utilizam o sistema operacional Linux. Nesse projeto foi utilizado Yaml e shellscript para automatizar a instalação do setup e fazer as configurações se tornarem fáceis de instalar e rodar a última versão do Nglide 21 da Zeus software, possibilitando o jogo rodar com uma API mais moderna como o Vulkan";
+
+var ghud = document.querySelector(".ghud").innerHTML = "Carmageddon 2 rodando com Gallium Hud";
+
+
+
+
+
+
+
+
+
+var copy = document.querySelector(".copy").innerHTML = "Criado por Gustavo Ribeiro &copy; 2023";
